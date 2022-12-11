088e24d2bf63f19876b0bf5c105777ac1ea8f5b1
NourallahAhmed <87453033+NourallahAhmed@users.noreply.github.com>
2022-12-12 00:25:41 +0200

    adding streams in login view model

--- lib/presentation_layer/login_screen/viewmodel/login_viewmodel.dart
+++ lib/presentation_layer/login_screen/viewmodel/login_viewmodel.dart
@@ -1,25 +1,80 @@
-import 'package:dartz/dartz.dart';
-import 'package:tut_advanced_clean_arch/data_layer/data_source/network/failure.dart';
-import 'package:tut_advanced_clean_arch/data_layer/models/response_model/login_request.dart';
-import 'package:tut_advanced_clean_arch/domain_layer/model/login_models.dart';
-import 'package:tut_advanced_clean_arch/domain_layer/usecase/base/base_usecase.dart';
+import 'dart:async';
 import 'package:tut_advanced_clean_arch/presentation_layer/view_model_base/base_view_model.dart';
 
-class LoginViewModel implements BaseViewModel{
-  @override
-  void dispose() {
-    // TODO: implement dispose
-  }
+import '../../../domain_layer/usecase/login_usecase/login_usecase.dart';
 
-  @override
-  void start() {
-    // TODO: implement start
-  }
+class LoginViewModel implements BaseViewModel , LoginViewModelInput , LoginViewModelOutput{
+
+
+
+      // initialize streams
+
+      final StreamController _userNameStreamController  = StreamController<String>.broadcast(); /// broadcast for multipleListener
+      final StreamController _passwordStreamController  = StreamController<String>.broadcast(); /// broadcast for multipleListener
+
+      // useCase object
+      final LoginUseCase _loginUseCase;
+
+      LoginViewModel(this._loginUseCase);
+
+      @override
+      void dispose() {
+        _passwordStreamController.close();
+        _userNameStreamController.close();
+      }
+
+      @override
+      void start() {
+        // TODO: implement start
+      }
+
+
+      ///Input Actions
+      @override
+      login() {
+        // TODO: implement login
+        throw UnimplementedError();
+      }
+
+      @override
+      Sink get passwordInput => _passwordStreamController.sink ;
+
+      @override
+      Sink get userNameInput => _userNameStreamController.sink;
+
+      @override
+      setPassword(String password) {
+        // TODO: implement setPassword
+        throw UnimplementedError();
+      }
+
+      @override
+      setUserName(String userName) {
+        // TODO: implement setUserName
+        throw UnimplementedError();
+      }
+
+
+      /// OutPuts
+      @override
+      Stream<bool> get passwordValidation => _passwordStreamController.stream.map((password) => _isPasswordValid(password));
+
+
+      @override
+      Stream<bool> get userNameValidation => _userNameStreamController.stream.map((userName) => _isUserNameValid(userName));
+
+      bool _isPasswordValid(String password ) {
+        return  password.isNotEmpty;
+      }
+
+      bool _isUserNameValid(String userName) {
+        return  userName.isNotEmpty;
+      }
 
 }
 
 
-abstract class LoginViewModelInput{
+abstract class LoginViewModelInput {
 
   /// Actions:
   /// user enter UserName
@@ -35,10 +90,16 @@ abstract class LoginViewModelInput{
 
 
   /// work with streams
-  Sink  get _loginViewModelInput;
-}
-abstract class LoginViewModelOutput{
+  Sink get passwordInput;
 
+  Sink get userNameInput;
+}
+
+abstract class LoginViewModelOutput {
+  /// work with streams
+  Stream <bool> get passwordValidation;
+
+  Stream <bool> get userNameValidation;
 }
 
 
