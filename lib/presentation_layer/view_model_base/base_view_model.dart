c3dceb1684ee1aaa7bda9ee2b32a5be4f1d090ff
NourallahAhmed <87453033+NourallahAhmed@users.noreply.github.com>
2022-12-23 21:08:58 +0200

    add stream controller for flowstate in base viewmodel to be accessible for all viewmodels

--- lib/presentation_layer/view_model_base/base_view_model.dart
+++ lib/presentation_layer/view_model_base/base_view_model.dart
@@ -1,15 +1,36 @@
+import 'dart:async';
+
+
+import '../common/state_randerer/state_renderer_impl.dart';
+
 abstract class BaseViewModel extends BaseViewModelInputs with BaseViewModeOutputs{
 
+  //stream for the flowState for all viewModels
 
+  final StreamController<FlowState> _flowStateStreamController = StreamController.broadcast();
+
+  @override
+  dispose() {
+    _flowStateStreamController.close();
+  }
+
+  @override
+  Sink<FlowState> inputFlowState() => _flowStateStreamController.sink;
+
+  @override
+  Stream<FlowState> outputFlowState() =>
+      _flowStateStreamController.stream.map((flowState) => flowState);
 }
 
 abstract class BaseViewModelInputs{
   void start();
   void dispose();
+  Sink<FlowState> inputFlowState();
 }
 
 
 abstract class BaseViewModeOutputs{
+  Stream<FlowState> outputFlowState();
 
 }
 
